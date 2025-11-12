6ebb3994da2c2a41d908877f304a3a87c54fea07
Monica Aichouri <69013706+mgu0625@users.noreply.github.com>
2025-11-11 20:38:41 -0500

    Lesson 30

---
+++ NodeAndThreeJS_Projects/Lesson30/src/shaders/galaxy/vertex.glsl
@@ -0,0 +1,46 @@
+uniform float uTime;
+uniform float uSize;
+
+attribute float aScale;
+attribute vec3 aRandomness;
+
+varying vec3 vColor;
+
+void main()
+{
+    /**
+     * position
+     */
+    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    // modelPosition.y += uTime;
+
+    //spin
+    float angle = atan(modelPosition.x, modelPosition.z);
+    float distanceToCenter = length(modelPosition.xz);
+    float angleOffset = (1.0 / distanceToCenter) * uTime * 0.2;
+    angle += angleOffset;
+    modelPosition.x = cos(angle) * distanceToCenter;
+    modelPosition.z = sin(angle) * distanceToCenter;
+
+    // randomness
+    // modelPosition.x += aRandomness.x;
+    // modelPosition.y += aRandomness.y;
+    // modelPosition.z += aRandomness.z;
+    modelPosition.xyz += aRandomness;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+    gl_Position = projectedPosition;
+
+    /**
+     * sizes
+     */
+    gl_PointSize = uSize * aScale;
+    gl_PointSize *= (1.0 / - viewPosition.z );
+
+    /**
+     * color
+     */
+    vColor = color;
+    
+}
