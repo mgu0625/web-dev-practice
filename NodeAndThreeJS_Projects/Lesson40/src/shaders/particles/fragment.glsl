0ac97c3d63ade0f59f37ff337e9ca218a2c75462
Monica Aichouri <69013706+mgu0625@users.noreply.github.com>
2025-11-14 20:03:48 -0500

    Lesson 40

---
+++ NodeAndThreeJS_Projects/Lesson40/src/shaders/particles/fragment.glsl
@@ -0,0 +1,17 @@
+varying vec3 vColor;
+
+void main()
+{
+    vec2 uv = gl_PointCoord;
+    float distanceToCenter = length(uv - 0.5);
+    float alpha = 0.05 / distanceToCenter - 0.1;
+
+    // gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+    // gl_FragColor = vec4(uv, 1.0, 1.0);
+    // gl_FragColor = vec4(distanceToCenter, distanceToCenter, distanceToCenter, 1.0);
+    // gl_FragColor = vec4(alpha, alpha, alpha, 1.0);
+    // gl_FragColor = vec4(1.0, 1.0, 1.0, alpha);
+    gl_FragColor = vec4(vColor, alpha);
+    #include <tonemapping_fragment>
+    #include <colorspace_fragment>
+}
